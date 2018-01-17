b4a57715b30b1dab9eed650b072ffc5c11af9245
Julian Pszczołowski <julian.pszczolowski@gmail.com>
2018-01-17 13:21:29 +0100

    added vector prints

--- src/helper.ml
+++ src/helper.ml
@@ -10,3 +10,12 @@ let quadratic_equation_roots a b c =
 let rec list_mult_plus (a : vector) (b : vector) = function
   | [] -> []
   | h::t -> ((a#mult h)#plus b)::list_mult_plus a b t
+
+let print_vector (v : vector) =
+  print_string "("; print_float v#x; print_string ", ";
+  print_float v#y; print_string ", ";
+  print_float v#z; print_string ")"
+
+let rec print_vector_list = function
+  | [] -> ()
+  | h::t -> print_vector h; print_string " "; print_vector_list t
